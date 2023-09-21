09b1734660193b0bce25fcb1e35b5a01cb54e3fb
angleixx <angielxx94@gmail.com>
2023-09-21 23:43:22 +0900

    Feat: Users 모델 정의

---
+++ models/user.ts
@@ -0,0 +1,58 @@
+// models/user.js
+
+import Sequelize, {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare user_id: CreationOptional<number>;
+  declare email: string;
+  declare password: string;
+  declare username: string;
+  declare created_at: CreationOptional<Date>;
+
+  static initiate(sequelize: Sequelize.Sequelize) {
+    User.init(
+      {
+        user_id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        username: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+
+        created_at: DataTypes.DATE,
+      },
+      {
+        sequelize,
+        modelName: 'Users',
+        tableName: 'users',
+        paranoid: true,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        createdAt: 'created_at',
+        updatedAt: false,
+        deletedAt: false,
+      }
+    );
+  }
+
+  // static associate(db) {}
+}
+
+module.exports = User;
